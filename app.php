561ba2a2f62356c3aae6eaedf03215b7ef084b80
designali32 <alicengelcik@gmail.com>
2021-08-19 23:13:40 +0300

    homework_1_1

--- app.php
+++ app.php
@@ -1,7 +1,24 @@
 <?php
 
-$age = 1256;
-
+$age = 25;
+$age=(int) $age;
+//age değişkeni int gelmeme ihtimaline karşı temizlendi.
+//yaş aralıklarına uygun şekilde ekrana veri basılıyor
+if($age>=0 and $age<=14){
+    echo 'Çocuk';
+}
+else if($age>=15 and $age<=24){
+    echo 'Genç';
+}
+else if($age>=25 and $age<=64){
+    echo "Yetişkin";
+}
+else if($age>=65){
+    echo "Yaşlı";
+}
+else{
+    echo "\$age değişkenine girilen veri yaş sınırlarına uygun değildir.";
+}
 /**
  * Bu ödevde age değişkeni içerisinde yer alan yaş değerinin bulunduğu
  * yaş grubu tespit edilip ekrana bu yaş grubunun ismi yazılmalıdır. 
@@ -18,21 +35,3 @@ $age = 1256;
  * 
  * İyi çalışmalar.
  */
-$age=(int) $age;
-//age değişkeni int gelmeme ihtimaline karşı temizlendi.
-//yaş aralıklarına uygun şekilde ekrana veri basıldı.
-if($age>=0 and $age<=14){
-    echo 'Çocuk';
-}
-else if($age>=15 and $age<=24){
-    echo 'Genç';
-}
-else if($age>=25 and $age<=64){
-    echo "Yetişkin";
-}
-else if($age>=65){
-    echo "Yaşlı";
-}
-else{
-    echo "\$age değişkenine girilen veri yaş sınırlarına uygun değildir.";
-}
